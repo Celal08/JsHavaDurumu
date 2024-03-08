7b782d3052cfbd70ee2f442e4496b7b1e2124afc
Celal08 <cesey08@gmail.com>
2024-03-08 21:34:53 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,51 @@
+const cityInput = document.querySelector(".inputText")
+const btn = document.querySelector(".btn")
+
+
+//addEventListener Olay ne olduğunda gerçekleşecek, Olay gerçekleştikten sonra ne olacak
+btn.addEventListener("click",()=>{
+    // console.dir(cityInput)
+    const cityName=  cityInput.value
+    getData(cityName)
+
+})
+
+function getData(name){
+    
+   const API ="53cd16e1e05751f0c2eea6b01c845ccb";
+   const baseURL=`https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${API}&units=metric&lang=tr`;
+//    console.log(fetch(baseURL))
+fetch(baseURL)
+.then(res => res.json())
+.then(data => {
+    const {name, sys:{country},main: {temp,feels_like, humidity},weather: [{description}],wind: {speed}} = data;
+    console.log(name,country,temp,feels_like,humidity,description,speed)
+
+    //verileri JS'ye çekme
+
+    const city = document.querySelector(".city")
+    const temperature = document.querySelector(".temp")
+    const hum = document.querySelector(".humidity")
+    const wind = document.querySelector(".wind")
+    const weather = document.querySelector(".weather")
+    const feeling = document.querySelector(".feeling")
+    console.log(city,temperature,hum,wind,weather,feeling)
+
+    //JS'e çekilen elemanları yerine yerleştirme
+    city.textContent = `${name},${country}`
+    temperature.innerText = `${temp.toFixed(0)}°`
+    hum.textContent = `Nem: %${humidity}°`
+    wind.innerHTML=`Rüzgar: ${speed}km/s`;
+    weather.innerHTML = `<i>${description.toUpperCase()}</i>`
+    feeling.textContent = `Hissedilen: ${feels_like}°`
+
+   
+
+})
+.catch(err => console.log(err))
+//input un içini boşaltır
+cityInput.value="";
+
+//input a focuslar
+cityInput.focus();
+}
